783b0ce965139e6fe4e8cf21bbeb064502b2d593
Zhen Lee <zhenlee105@gmail.com>
2017-07-27 19:11:42 +0800

    ...

--- utils.py
+++ utils.py
@@ -113,14 +113,14 @@ def next_batch(sess, model, data_dir, h5File, idList):
             img3 = load_image("%s/%s.jpg" % (data_dir, idList[pos3[j]]))
             img4 = load_image("%s/%s.jpg" % (data_dir, idList[pos4[j]]))
             batch = tf.stack([img1, img2, img3, img4])
-            labels[:,j,:] = sess.run(model.vlad_output, feed_dict = {'query_image': batch, 'train_mode' : False})
+            labels[:,j,:] = sess.run(model.vlad_output, feed_dict = {'query_image:0': batch, 'train_mode:0' : False})
         for k in range(20):
             img1 = load_image("%s/%s.jpg" % (data_dir, idList[neg1[k]]))
             img2 = load_image("%s/%s.jpg" % (data_dir, idList[neg2[k]]))
             img3 = load_image("%s/%s.jpg" % (data_dir, idList[neg3[k]]))
             img4 = load_image("%s/%s.jpg" % (data_dir, idList[neg4[k]]))
             batch = tf.stack([img1, img2, img3, img4])
-            labels[:,k + 10,:] = sess.run(model.vlad_output, feed_dict = {'query_image': batch, 'train_mode' : False})
+            labels[:,k + 10,:] = sess.run(model.vlad_output, feed_dict = {'query_image:0': batch, 'train_mode:0' : False})
 
         yield x, labels, z
 
