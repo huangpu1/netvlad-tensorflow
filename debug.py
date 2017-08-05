94cad25e08ead71a76da13c0189c257b8baedb66
Zhen Lee <zhenlee105@gmail.com>
2017-08-05 19:59:29 +0800

    rmspropoptimizer

--- debug.py
+++ debug.py
@@ -1,6 +1,6 @@
 import eva_utils
 import eva_init
 
-qList, dbList = eva_init.get_List("tokyoTM/tokyoTM_val.mat")
+qList, dbList = eva_init.get_List("tokyoTM/tokyoTM_train.mat")
 h5File = 'index/evadata.hdf5'
 eva_utils.debug(h5File, qList, dbList)
