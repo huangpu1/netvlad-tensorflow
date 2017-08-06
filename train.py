4fad08adce36a9d36e9e5a787b15ba529fcf659d
Zhen Lee <zhenlee105@gmail.com>
2017-08-06 17:23:15 +0800

    rmspropoptimizer

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def triplet_loss(q, labels, m):
     L2_distance = tf.norm(tf.subtract(tf.expand_dims(q, axis = -1), labels), axis = 1)
     positives, negatives = tf.split(L2_distance, [40, 20], axis = 1)
     if FLAGS.useRelu:
-        loss = tf.reduce_sum(tf.nn.relu(tf.reduce_min(positives, axis = -1, keep_dims = True) + m - negatives))
+        loss = tf.reduce_sum(tf.nn.relu(tf.subtract(tf.add(tf.reduce_min(positives, axis = -1, keep_dims = True), m), negatives)))
     else:
         loss = tf.reduce_sum(tf.reduce_min(positives, axis = -1, keep_dims = True) + m - negatives)
     # loss = tf.reduce_sum(tf.nn.relu(tf.reduce_min(positives, axis = -1, keep_dims = True) + m - negatives))
